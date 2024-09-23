1891963bd7a0d26dac3227ad84a666dfb27f5ebb
HafsaAbouyassine <101837805+HafsaAbouyassine@users.noreply.github.com>
2024-09-23 16:10:34 +0200

    les class en Doctrine
    identification des class (users, measurements) en Doctrine et en php

---
+++ measurements.class.php
@@ -0,0 +1,39 @@
+<?php
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/** 
+ * @Entity
+ * @Table(name="uapv2100281.measurements")
+ */
+class Measurement {
+
+    /** @Id @Column(type="integer")
+     *  @GeneratedValue
+     */ 
+    public $id;
+
+    /** 
+     * @ManyToOne(targetEntity="User", inversedBy="measurements")
+     * @JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    public $user;
+
+    /** @Column(type="decimal", precision=5, scale=2) */ 
+    public $weight;
+
+    /** @Column(type="decimal", precision=4, scale=2) */ 
+    public $height;
+
+    /** @Column(type="decimal", precision=4, scale=2) */ 
+    public $calculated_imc;
+
+    /** @Column(type="datetime") */ 
+    public $taken_at;
+
+    /*public function __construct() {
+        $this->taken_at = new \DateTime(); // Par défaut, la date est la date actuelle
+    }*/
+}
+
+?>
