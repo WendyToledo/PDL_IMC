8f7015447d3432cef591ab42e502152e8f20d2b1
Gwanaelle Davesne <145539127+GwanaelleDavesne@users.noreply.github.com>
2024-09-30 15:41:05 +0200

    ajout formulaire imc

--- layout.php
+++ layout.php
@@ -4,6 +4,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+		   <script src="js/soumissionConnexion.js"></script>
 		<title> Calcul d'IMC </title>
 		<style>
 			body {
@@ -27,6 +28,11 @@
 			}
 			input[type=submit]:hover {
 				background-color: #6495ED;
+			}
+			.container {
+				border-radius: 5px;
+				background-color: #f2f2f2;
+				padding: 20px;
 			}
 			.bandeau {
 				background-color: #6495ED;
@@ -76,25 +82,35 @@
 			.bouton:hover {
 				background-color: #B0C4DE;
 			}
+			.input {
+            position: relative;
+            display: inline-block;
+			}
+
+			.input .unit {
+				position: absolute;
+				right: 10px;
+				top: 45%;
+				color: #000000;
+			}
 		</style>
 	</head>
 
 	<body>
 		
-<div class="bandeau"> 
+		<div class="bandeau"> 
 			<h1>Calcul d'IMC </h1>
-			<?php if(isset($_SESSION['id'])) {
-		echo('<button id="boutons" onclick="deco()" "id="deconnexionBtn" class="bouton">Deconnexion</button>');
-	}else{
-       		echo ('<button id="boutons" onclick="co()" "id="connexionBtn" class="bouton">Connexion</button>');
-	} ?>
-			
+			<div id="boutons">
+    <?php if(isset($_SESSION['id'])) {
+        echo('<button id="deconnexionBtn" onclick="deco()" class="bouton">Déconnexion</button>');
+    } else {
+        echo ('<button id="connexionBtn" onclick="co()" class="bouton">Connexion</button>');
+    } ?>
+</div>
 		</div>
-<?php if(isset($_SESSION['id'])) {
-		echo 'Bienvenu '.$_SESSION['id'];
-	} ?>
-
-
+		<?php if(isset($_SESSION['id'])) {
+			echo 'Bienvenu '.$_SESSION['id'];
+		} ?>
 
 		<div id="connexionModal" class="modal">
 			<div class="modal-content">
@@ -109,23 +125,45 @@
 				</form>
 			</div>
 		</div>
+		<br><br>
+		<div id="page_maincontent">	
+				<div class="container">
+					<form id="form_imc" method="POST">
+						<div class="input">
+							<label for="taille">Taille</label>
+							<input type="text" name="taille" id="taille" placeholder="Indiquer votre taille" required oninput="notLetter(this)">
+							<span class="unit">cm</span>
+						</div>
+						<br><br>
+						<div class="input">	
+							<label for="depart">Poids</label>
+							<input type="text" name="poids" id="poids" placeholder="Indiquer votre poids" required oninput="notLetter(this)">
+							<span class="unit">kg</span>
+						</div>
+						<br><br>
+						<input type="submit" value="Calculer">
+					</form>
+				</div>
+		</div>
 	</body>
 	<script>
 		function deco () {
-			
-	<?php
+			<?php
+				// Destroying the session
+				session_destroy();
+				unset($_SESSION['id']);
+			?>
+			location.reload();
+		};
 
-
-	// Destroying the session
-	session_destroy();
-	unset($_SESSION['id']);
-
-?>
-location.reload();
-};
-
-function co () {
-	document.getElementById("connexionModal").style.display = "flex";
-};
+		function co () {
+			document.getElementById("connexionModal").style.display = "flex";
+		};
+		
+		function notLetter(input) {
+			//empeche l'utilisateur de taper des lettres
+            input.value = input.value.replace(/[^\d]/g, '');
+        };
+		
 	</script>
 </html>
