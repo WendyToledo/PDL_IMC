ca179f9ef6941abc1494a9c62354439144131cd7
Gwanaelle Davesne <145539127+GwanaelleDavesne@users.noreply.github.com>
2024-11-18 17:12:10 +0100

    sprint 4

---
+++ sprint4/moduleConnexionSuccess.php
@@ -0,0 +1,13 @@
+<script src="js/soumissionDeconnexion.js"></script>
+utilisateur <?php echo $context->utilisateur->email?> connecté<br><br>
+
+<button id="deconnexionBtn" class="bouton">Déconnexion</button>
+
+<script>
+	//vider le formulaire de connexion
+    document.getElementById('form_connexion').reset();
+</script>
+
+
+
+
